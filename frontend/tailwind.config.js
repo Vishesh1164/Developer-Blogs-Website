d0e8f78f6795663f4e1a203e4c9f072f4e6ff2cd
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-06-08 17:42:47 +0530

    c

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  theme:  {
     extend: {
       colors: {
         background: "var(--background)",
