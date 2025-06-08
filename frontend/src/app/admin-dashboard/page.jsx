321bb6d97b8615f628745b3a5b481e62e615f002
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-06-08 17:29:50 +0530

    cors

--- frontend/src/app/admin-dashboard/page.jsx
+++ frontend/src/app/admin-dashboard/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { IconUsers, IconMessageCircle, IconEdit } from '@tabler/icons-react'
 import { useRouter } from 'next/navigation'
